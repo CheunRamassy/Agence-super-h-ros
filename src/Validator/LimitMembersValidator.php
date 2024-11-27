c72a9f33c033b05bc26dd060f6361be99af8807a
CheunRamassy <cheunramassy.pr@gmail.com>
2024-11-27 14:40:06 +0400

    Mise en place de la contrainte sur les membres (non fonctionnel)

---
+++ src/Validator/LimitMembersValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class LimitMembersValidator extends ConstraintValidator
+{
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        /* @var LimitMembers $constraint */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (Length($constraint->equipe) > constraint->max && length(constraint->equipe) < constraint->min){
+            $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+        }
+
+
+    }
+}
